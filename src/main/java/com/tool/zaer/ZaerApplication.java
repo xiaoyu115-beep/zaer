20136d2be2529371bfc8e81707db902595f538bc
xiaoYu <16673192830@163.com>
2025-02-18 15:37:25 +0800

    base

---
+++ src/main/java/com/tool/zaer/ZaerApplication.java
@@ -0,0 +1,13 @@
+package com.tool.zaer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tool/zaer/ZaerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.zaer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
