a9d58f4a15be646f671ecbbb5fbd6877cbbff308
xiaoYu <16673192830@163.com>
2025-02-19 20:20:28 +0800

    base

---
+++ redisjava/src/main/java/com/tool/redisjava/RedisjavaApplication.java
@@ -0,0 +1,13 @@
+package com.tool.redisjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisjavaApplication.class, args);
+    }
+
+}
---
+++ redisjava/src/test/java/com/tool/redisjava/RedisjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.redisjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
